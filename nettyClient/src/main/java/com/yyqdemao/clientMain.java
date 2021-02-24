29e655a49e89c4870a92a8c3dc40acdcc275a68b
邬嘉晖 <2591981044@qq.com>
2021-02-24 17:02:47 +0800

    Initial commit

---
+++ nettyClient/src/main/java/com/yyqdemao/clientMain.java
@@ -0,0 +1,13 @@
+package com.yyqdemao;
+
+public class clientMain {
+    public static void main(String[] args) {
+        if(args.length>0){
+            for (String s:args) {
+               if("-help".equals(s)){
+
+               }
+            }
+        }
+    }
+}
