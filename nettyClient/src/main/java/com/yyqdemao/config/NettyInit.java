b078691d9337b2ba74e28a60fb99923e8b742e85
邬嘉晖 <2591981044@qq.com>
2021-03-04 16:55:11 +0800

    protobuf 传输对象

--- nettyClient/src/main/java/com/yyqdemao/config/NettyInit.java
+++ nettyClient/src/main/java/com/yyqdemao/config/NettyInit.java
@@ -4,6 +4,8 @@ import io.netty.channel.ChannelInitializer;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.protobuf.ProtobufDecoder;
 import io.netty.handler.codec.protobuf.ProtobufEncoder;
+import io.netty.handler.codec.protobuf.ProtobufVarint32FrameDecoder;
+import io.netty.handler.codec.protobuf.ProtobufVarint32LengthFieldPrepender;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
 
@@ -13,8 +15,10 @@ public class NettyInit extends ChannelInitializer<SocketChannel> {
     @Override
     protected void initChannel(SocketChannel socketChannel) throws Exception {
         socketChannel.pipeline()
-                .addLast("encoder",new ProtobufEncoder())
-                .addLast("decoder",new ProtobufDecoder(StudentPOJO.MyMessage.getDefaultInstance()))
+                .addLast(new ProtobufVarint32FrameDecoder())//添加protobuff解码器
+                .addLast("decoder", new ProtobufDecoder(StudentPOJO.MyMessage.getDefaultInstance()))
+                .addLast(new ProtobufVarint32LengthFieldPrepender())
+                .addLast("encoder", new ProtobufEncoder())
                 .addLast(new NettyHandler());
 //        socketChannel.pipeline().addLast(new NettyHandler());
     }
--- nettyServer/src/main/java/com/yyqdemao/config/NettyInit.java
+++ nettyServer/src/main/java/com/yyqdemao/config/NettyInit.java
@@ -4,6 +4,8 @@ import io.netty.channel.ChannelInitializer;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.protobuf.ProtobufDecoder;
 import io.netty.handler.codec.protobuf.ProtobufEncoder;
+import io.netty.handler.codec.protobuf.ProtobufVarint32FrameDecoder;
+import io.netty.handler.codec.protobuf.ProtobufVarint32LengthFieldPrepender;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
 import io.netty.handler.timeout.IdleStateHandler;
@@ -16,8 +18,10 @@ public class NettyInit extends ChannelInitializer<SocketChannel> {
     protected void initChannel(SocketChannel socketChannel) throws Exception {
         socketChannel.pipeline()
                 //指定解码对象
+                .addLast(new ProtobufVarint32FrameDecoder())//添加protobuff解码器
                 .addLast("decoder", new ProtobufDecoder(StudentPOJO.MyMessage.getDefaultInstance()))
-                .addLast("edcoder",new ProtobufEncoder())
+                .addLast(new ProtobufVarint32LengthFieldPrepender())
+                .addLast("encoder", new ProtobufEncoder())
 //                .addLast("decoder",new StringDecoder(Charset.forName("UTF-8")))
 //                .addLast("encoder",new StringEncoder(Charset.forName("UTF-8")))
                 .addLast(new IdleStateHandler(2,2,2, TimeUnit.MINUTES))
